f1e4936bde5e89ab03a134978788864df7f0df9f
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-07-25 08:29:23 -0300

    feat: add task DTO

---
+++ Domain/Dtos/TaskDTO.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums;
+
+namespace Domain.Dtos;
+public class TaskDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public PriorityEnum Priority { get; set; }
+    public DateTime LimitDate { get; set; }
+    public StatusEnum Status { get; set; }
+}
+
+public class UpdateTaskDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public PriorityEnum Priority { get; set; }
+    public DateTime LimitDate { get; set; }
+    public StatusEnum Status { get; set; }
+}
