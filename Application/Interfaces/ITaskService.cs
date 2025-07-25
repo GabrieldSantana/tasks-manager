99364afcb015bf59fbf48a914494dc29bc411978
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-07-25 08:25:32 -0300

    feat: add Task Service Interface

---
+++ Application/Interfaces/ITaskService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Dtos;
+using Domain.Models;
+
+namespace Application.Interfaces;
+public interface ITaskService
+{
+    Task<List<TaskModel>> GetTasksAsync();
+    Task<TaskModel> GetTaskByIdAsync(int id);
+    Task<bool> CreateTaskAsync(TaskDTO taskDTO);
+    Task<bool> UpdateTaskAsync(UpdateTaskDTO taskDTO);
+    Task<bool> DeleteTaskAsync(int id);
+}
